a4ef88b12ec80046e7c8b5a306a7bad359db0aa7
Eliza Brock <eliza@elizabrocksoftware.com>
2012-10-23 10:26:16 -0500

    Files generated by `rails new -T -d postgresql`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weeblog::Application.config.secret_token = '7d36a0341dfe8ea484f6b51ffc35541769afaff26f1c9ab6a668530e4b3b425e7b97b4125ce54750c3f66eba47585f1c088e12e697f6c34f8c00301b7612533c'
