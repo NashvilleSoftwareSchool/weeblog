e7266646a61959925f2add8fe9024ca00418fd30
Eliza Brock <eliza@elizabrocksoftware.com>
2012-10-23 11:13:50 -0500

    Public controller ("Hello, World")

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,5 @@
+class PublicController < ApplicationController
+  def index
+    render text: "Hello, world!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Weeblog::Application.routes.draw do
+  root to: "public#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
