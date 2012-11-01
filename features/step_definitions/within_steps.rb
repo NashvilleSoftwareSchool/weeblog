0b57434757ff75ad7eb4b0be9fc2bf7d615d5226
Eliza Brock <eliza@elizabrocksoftware.com>
2012-11-01 09:23:02 -0500

    Within steps

---
+++ features/step_definitions/within_steps.rb
@@ -0,0 +1,37 @@
+require 'uri'
+require 'cgi'
+
+def selector_for(description)
+  case description
+  when "the results section"
+    'section.results'
+  else
+    description.gsub('"','')
+  end
+end
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
+# Single-line step scoper
+When /^(.*) within ([^:]*)$/ do |stepdescription, parent|
+  if parent == "the iframe"
+    page.driver.within_frame(page.find("iframe")["id"]) { step stepdescription }
+  else
+    with_scope(parent) { step stepdescription }
+  end
+end
+
+# Multi-line step scoper
+When /^(.*) within (.*[^:]):$/ do |stepdescription, parent, table_or_string|
+  if parent == "the iframe"
+    page.driver.within_frame(page.find("iframe")["id"]) { step "#{stepdescription}:", table_or_string }
+  else
+    with_scope(parent) { step "#{stepdescription}:", table_or_string }
+  end
+end
+
