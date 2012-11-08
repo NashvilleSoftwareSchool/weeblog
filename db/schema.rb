38711cdff027e8cba36962c8b7f1949a1659bea7
Keith Marcum <marcum.keith@gmail.com>
2012-11-08 11:06:04 -0600

    Add missing migration for parent_comment_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106174159) do
+ActiveRecord::Schema.define(:version => 20121108170416) do
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
