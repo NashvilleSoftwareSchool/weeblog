2720c48f9ee6dbf88a82df1eaa8cf3477139596c
Reed Stinson <reed.stinson@gmail.com>
2012-11-14 10:12:06 -0600

    trying o git rid of unwanted files

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(:version => 20121107164422) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "pages", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
