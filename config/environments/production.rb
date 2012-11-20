4e016d510b6e1514ce365051b2b77c663cf45bff
Eliza Brock <eliza@elizabrocksoftware.com>
2012-11-20 11:07:32 -0600

    Production asset settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Weeblog::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -47,7 +48,7 @@ Weeblog::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
- config.assets.precompile += %w( modernizr.js )
+ config.assets.precompile += %w( application.css modernizr.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
