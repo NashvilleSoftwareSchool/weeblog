d2bfedffa899ccf05bb9048b5f7b361e27446327
Eliza Brock <eliza@elizabrocksoftware.com>
2012-11-20 11:55:12 -0600

    Move initialize_on_precompile to application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,6 @@ Weeblog::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -48,7 +47,7 @@ Weeblog::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
- config.assets.precompile += %w( application.css modernizr.js )
+ config.assets.precompile += %w( modernizr.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
